6132ecf0062417bce75e2ca6041f244399c11997
elias <provinciaelias@gmail.com>
2024-02-20 08:57:18 -0500

    feat : add JwtService

--- src/main/java/com/elias/springboot3jwtsecurity/config/JwtService.java
+++ src/main/java/com/elias/springboot3jwtsecurity/config/JwtService.java
@@ -1,11 +1,41 @@
 package com.elias.springboot3jwtsecurity.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    public String extractUsername(String token) {
-        return null;
+    private static final String SECRET_KEY="qSlVSbcWkX+wuZHOji1LDndNRzIEtY/HTzCPeCVv6sR7as0npPDps8VqgXLLPcwH\n";
+    public String extractUsername(String token)
+    {
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimResolver)
+    {
+        final Claims claims=extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token)
+    {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+
     }
 }
