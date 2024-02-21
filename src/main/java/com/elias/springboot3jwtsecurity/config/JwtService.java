21ff01520e285c3d5ac6b63743524200a60ab26f
elias <provinciaelias@gmail.com>
2024-02-21 09:00:16 -0500

    feat: generate token

--- src/main/java/com/elias/springboot3jwtsecurity/config/JwtService.java
+++ src/main/java/com/elias/springboot3jwtsecurity/config/JwtService.java
@@ -2,11 +2,15 @@ package com.elias.springboot3jwtsecurity.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -23,6 +27,28 @@ public class JwtService {
         final Claims claims=extractAllClaims(token);
         return claimResolver.apply(claims);
     }
+
+    public String generateToken(UserDetails userDetails)
+    {
+        return generateToken(Map.of(),userDetails);
+    }
+
+    public String generateToken(
+            Map<String,Object> extraClaims,
+            UserDetails userDetails
+    )
+    {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+
 
     private Claims extractAllClaims(String token)
     {
