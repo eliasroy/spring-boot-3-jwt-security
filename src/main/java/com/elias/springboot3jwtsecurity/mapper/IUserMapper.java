bd2c1262182fac475cb8fa376fd3b75778a48be1
elias <provinciaelias@gmail.com>
2024-02-22 08:43:16 -0500

    fix: add bean mapper

--- src/main/java/com/elias/springboot3jwtsecurity/mapper/IUserMapper.java
+++ src/main/java/com/elias/springboot3jwtsecurity/mapper/IUserMapper.java
@@ -3,11 +3,12 @@ package com.elias.springboot3jwtsecurity.mapper;
 import com.elias.springboot3jwtsecurity.user.User;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Optional;
-
+@Mapper
 public interface IUserMapper {
     @Select("SELECT * FROM USER WHERE email=#{email}")
     Optional<User> findByEmail(String email);
