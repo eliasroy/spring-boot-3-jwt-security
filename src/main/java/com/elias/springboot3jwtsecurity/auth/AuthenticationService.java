44e3952539e30a74035d8a092f7d6f122ce9c462
elias <provinciaelias@gmail.com>
2024-02-24 13:35:57 -0500

    feat add class service

---
+++ src/main/java/com/elias/springboot3jwtsecurity/auth/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.elias.springboot3jwtsecurity.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+}
