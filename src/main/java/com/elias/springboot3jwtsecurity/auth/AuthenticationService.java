7b07950d06012470bdf5e909f27eb5b4ad3b4755
elias <provinciaelias@gmail.com>
2024-02-28 07:37:03 -0500

    add method authenticate

--- src/main/java/com/elias/springboot3jwtsecurity/auth/AuthenticationService.java
+++ src/main/java/com/elias/springboot3jwtsecurity/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.elias.springboot3jwtsecurity.mapper.IUserMapper;
 import com.elias.springboot3jwtsecurity.user.Role;
 import com.elias.springboot3jwtsecurity.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ public class AuthenticationService {
     private final IUserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthehenticationResponse register(RegisternRequest request) {
         var user = User.builder()
@@ -33,6 +36,17 @@ public class AuthenticationService {
     }
 
     public AuthehenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userMapper.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthehenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
