ed424586eb651b6ef448e70e96ebedf113e77ee5
elias <provinciaelias@gmail.com>
2024-03-03 23:34:03 -0500

    change

--- src/main/java/com/elias/springboot3jwtsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/elias/springboot3jwtsecurity/config/SecurityConfiguration.java
@@ -36,8 +36,10 @@ public class SecurityConfiguration {
             "/webjars/**",
             "/swagger-ui.html"};
     private final JwtAuthenticationFilter jwtAuthFilter;
+
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
